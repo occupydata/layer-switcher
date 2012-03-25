57de6d93d213446131440eebae765bf2a00ed00a
Mike Morris <michael.patrick.morris@gmail.com>
2012-03-25 12:57:25 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+var map;
+
+var url = 'http://api.tiles.mapbox.com/v3/';
+var baselayer = "mapbox.mapbox-streets"
+var activelayer = "djohnson.romney-final";
+
+// Define a custom icon using the Maki museum icon
+var museumIcon = L.Icon.extend({
+  iconUrl: 'img/museum-24.png',
+  shadowUrl: null,
+  iconSize: new L.Point(24, 24),
+  shadowSize: null,
+  iconAnchor: new L.Point(12, 24),
+  popupAnchor: new L.Point(0,-24)
+});
+
+var geojsonLayer = new L.GeoJSON(data, {
+  pointToLayer: function (latlng) {
+    return new L.Marker(latlng, {
+      icon: new museumIcon()
+    });
+  }
+});
+
+wax.tilejson(url + baselayer + ',' + activelayer + '.jsonp', function(tilejson) {
+  map = new L.Map('map')
+  .setView(new L.LatLng(39, -78), 4)
+  .addLayer(new wax.leaf.connector(tilejson))
+  .addLayer(geojsonLayer);
+
+  tilejson.minzoom = 2;
+  tilejson.maxzoom = 7;
+
+  geojsonLayer.on('featureparse', function (e) {
+    if (e.properties && e.properties.name){
+      e.layer.bindPopup(e.properties.name);
+    }
+  });
+
+  geojsonLayer.addGeoJSON(data);
+});
+
+function refreshMap(layers) {
+  wax.tilejson('http://api.tiles.mapbox.com/v3/' + layers + '.jsonp', function (tilejson) {
+    map.addLayer(new wax.leaf.connector(tilejson));
+
+    tilejson.minzoom = 2;
+    tilejson.maxzoom = 7;
+  });
+}
+
+$(document).ready(function () {
+
+  $('ul.layers li a').click(function (e) {
+    e.preventDefault();
+    if (!$(this).hasClass('active')) {
+      $('ul.layers li a').removeClass('active');
+      $(this).addClass('active');
+      var activeLayer = $(this).attr('data-layer');
+      layers = [
+        baselayer,
+        activeLayer
+      ];
+      refreshMap(layers);
+    }
+  });
+
+});
---
+++ js/museums.js
@@ -0,0 +1,424 @@
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.06219, 38.72873]
+        }, 
+        "properties": {
+            "name" : "Carl Sanburg Middle School Planetarium"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.06775, 38.859]
+        }, 
+        "properties": {
+            "name" : "Arlington Historical Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.06747, 38.859]
+        }, 
+        "properties": {
+            "name" : "Hume Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.05802, 38.88095]
+        }, 
+        "properties": {
+            "name" : "Drug Enforcement Agency Museum and Visitors Center"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.06108, 38.90456]
+        }, 
+        "properties": {
+            "name" : "Museum of Contemporary Art District of Columbia"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.09886, 38.9215]
+        }, 
+        "properties": {
+            "name" : "Discovery Creek Children's Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.08858, 38.92206]
+        }, 
+        "properties": {
+            "name" : "Kreeger Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.05163, 38.914]
+        }, 
+        "properties": {
+            "name" : "Textile Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.04509, 38.7322]
+        }, 
+        "properties": {
+            "name" : "Collingwood Library and Museum on Americanism"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.04756, 38.80367]
+        }, 
+        "properties": {
+            "name" : "Lyceum Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02018, 38.88833]
+        }, 
+        "properties": {
+            "name" : "Albert Einstein Planetarium"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.0483, 38.89372]
+        }, 
+        "properties": {
+            "name" : "Diplomatic Reception Rooms Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.04024, 38.89483]
+        }, 
+        "properties": {
+            "name" : "National Society of Daughters of American Revolution"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.03274, 38.89428]
+        }, 
+        "properties": {
+            "name" : "National Aquarium of Washington District of Columbia"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.01934, 38.89312]
+        }, 
+        "properties": {
+            "name" : "Newseum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.00135, 38.89122]
+        }, 
+        "properties": {
+            "name" : "National Museum of African Art"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.03808, 38.90521]
+        }, 
+        "properties": {
+            "name" : "National Geographic Society"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.0383, 38.90622]
+        }, 
+        "properties": {
+            "name" : "B'Nai B'Rith Klutznick Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.04747, 38.91094]
+        }, 
+        "properties": {
+            "name" : "Anderson House Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.04684, 38.9115]
+        }, 
+        "properties": {
+            "name" : "The Phillips Collection"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.0421, 38.91283]
+        }, 
+        "properties": {
+            "name" : "National Museum Of American Jewish Military History"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.03552, 38.91372]
+        }, 
+        "properties": {
+            "name" : "House of the Temple Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.01964, 38.8963]
+        }, 
+        "properties": {
+            "name" : "Marian Koshland Science Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02168, 38.89664]
+        }, 
+        "properties": {
+            "name" : "National Museum of Crime and Punishment"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02334, 38.89703]
+        }, 
+        "properties": {
+            "name" : "International Spy Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02622, 38.8976]
+        }, 
+        "properties": {
+            "name" : "Madame Tussauds"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02245, 38.89782]
+        }, 
+        "properties": {
+            "name" : "National Portrait Gallery"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02352, 38.89783]
+        }, 
+        "properties": {
+            "name" : "Smithsonian American Art Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02941, 38.90011]
+        }, 
+        "properties": {
+            "name" : "National Museum of Women in the Arts"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02764, 38.90578]
+        }, 
+        "properties": {
+            "name" : "Laogai Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.03108, 38.90789]
+        }, 
+        "properties": {
+            "name" : "Bethune Museum-Archives"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.02469, 38.91733]
+        }, 
+        "properties": {
+            "name" : "Black Fashion Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.00812, 38.89768]
+        }, 
+        "properties": {
+            "name" : "National Postal Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.00828, 38.89774]
+        }, 
+        "properties": {
+            "name" : "National Philatelic and Postal History Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.01469, 38.89872]
+        }, 
+        "properties": {
+            "name" : "Lillian and Albert Small Jewish Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.01941, 38.92233]
+        }, 
+        "properties": {
+            "name" : "Howard University Museum;Moorland Spingarn Research Center"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.08627, 38.93873]
+        }, 
+        "properties": {
+            "name" : "American University Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.05261, 38.94363]
+        }, 
+        "properties": {
+            "name" : "Hillwood Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.05219, 38.94372]
+        }, 
+        "properties": {
+            "name" : "Hillwood Museum"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.05163, 38.95983]
+        }, 
+        "properties": {
+            "name" : "Rock Creek Park Nature Center and Planetarium"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.0324, 38.97646]
+        }, 
+        "properties": {
+            "name" : "National Museum of Health and Medicine"
+        }
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-77.03185, 39.09805]
+        }, 
+        "properties": {
+            "name" : "Trolly Car Museum"
+        }
+    }]
+};
