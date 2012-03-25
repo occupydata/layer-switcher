00650009ef97a16f2b2fd835163ce9db1e6c2cff
kleinjos <git@bf.33mail.com>
2012-03-25 18:39:00 -0400

    updated styling

--- js/app2.js
+++ js/app2.js
@@ -1,21 +1,25 @@
 var map;
 
 var url = 'http://api.tiles.mapbox.com/v2/';
-var baselayer = "occupy.background"
+var baselayer = "occupy.wht-us-base"
 var activelayer = "occupy.4q11-delinq";
 
-wax.tilejson(url + baselayer + ',' + activelayer + '.jsonp', function(tilejson) {
+wax.tilejson(url + baselayer + ',' + activelayer + ',occupy.state-lines' + '.jsonp', function(tilejson) {
   map = new L.Map('map')
   .setView(new L.LatLng(39, -78), 4)
   .addLayer(new wax.leaf.connector(tilejson));
+  wax.mm.interaction()
+    .map(map)
+    .tilejson(tilejson)
+    .on(wax.tooltip().animate(true).parent(map.parent).events());
 
   tilejson.minzoom = 2;
   tilejson.maxzoom = 7;
-
+  
 });
 
 function refreshMap(layers) {
-  wax.tilejson('http://api.tiles.mapbox.com/v2/' + layers + '.jsonp', function (tilejson) {
+  wax.tilejson('http://api.tiles.mapbox.com/v2/' + layers + ',occupy.state-lines' + '.jsonp', function (tilejson) {
     map.addLayer(new wax.leaf.connector(tilejson));
 
     tilejson.minzoom = 2;
