49a5048eb182063989d8cb4c0dad52cff6ac4748
Mike Morris <michael.patrick.morris@gmail.com>
2012-03-26 14:50:14 -0400

    adding leaflet submodule and geojson layer

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 var map, interactivity, layer;
 
 var url = 'http://api.tiles.mapbox.com/v3/';
-var baselayer = "occupy.wht-us-base"
+var baselayer = "mapbox.mapbox-streets";
 var activelayer = "occupy.4q11-delinq";
 var statelayer = "occupy.state-lines";
+
+// Define a custom icon using the Maki museum icon
+var museumIcon = L.Icon.extend({
+  iconUrl: 'img/museum-24.png',
+  shadowUrl: null,
+  iconSize: new L.Point(24, 24),
+  shadowSize: null,
+  iconAnchor: new L.Point(12, 24),
+  popupAnchor: new L.Point(0,-24)
+});
+
+// Define a geojson data layer
+  var geojsonLayer = new L.GeoJSON(data, {
+  pointToLayer: function (latlng) {
+    return new L.Marker(latlng, {
+      icon: new museumIcon()
+    });
+  }
+});
 
 wax.tilejson(url + baselayer + ',' + activelayer + ',' + statelayer + '.jsonp', function(tilejson) {
   map = new L.Map('map')
@@ -11,13 +30,23 @@ wax.tilejson(url + baselayer + ',' + activelayer + ',' + statelayer + '.jsonp', 
 
   layer = new wax.leaf.connector(tilejson);
   map.addLayer(layer);
+  map.addLayer(geojsonLayer);
+
+  tilejson.minzoom = 2;
+  tilejson.maxzoom = 7;
 
   interactivity = wax.leaf.interaction()
     .map(map)
     .tilejson(tilejson)
     .on(wax.tooltip().animate(true).parent(map._container).events());
-  tilejson.minzoom = 2;
-  tilejson.maxzoom = 7;
+
+  geojsonLayer.on('featureparse', function (e) {
+    if (e.properties && e.properties.name){
+      e.layer.bindPopup(e.properties.name);
+    }
+  });
+
+  geojsonLayer.addGeoJSON(data);
 });
 
 function refreshMap(layers) {
