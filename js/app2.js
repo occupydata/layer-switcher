8378a0fd7a716cb31fd5fd09e38c8dc352d871e3
Tom MacWright <tom@macwright.org>
2012-03-25 23:09:20 -0400

    Updated wax version, added tooltips

--- js/app2.js
+++ js/app2.js
@@ -1,34 +1,27 @@
-var map;
+var map, interactivity;
 
 var url = 'http://api.tiles.mapbox.com/v3/';
-var baselayer = "occupy.wht-us-base"
+var baselayer = "occupy.wht-us-base";
 var activelayer = "occupy.4q11-delinq";
 
 wax.tilejson(url + baselayer + ',' + activelayer + ',occupy.state-lines' + '.jsonp', function(tilejson) {
   map = new L.Map('map')
-  .setView(new L.LatLng(39, -78), 4)
-  .addLayer(new wax.leaf.connector(tilejson));
-  wax.leaf.interaction()
+    .setView(new L.LatLng(39, -78), 4)
+    .addLayer(new wax.leaf.connector(tilejson));
+  interactivity = wax.leaf.interaction()
     .map(map)
     .tilejson(tilejson)
     .on(wax.tooltip().animate(true).parent(map._container).events());
   tilejson.minzoom = 2;
   tilejson.maxzoom = 7;
-  
 });
 
 function refreshMap(layers) {
   wax.tilejson('http://api.tiles.mapbox.com/v3/' + layers + ',occupy.state-lines' + '.jsonp', function (tilejson) {
-    map.addLayer(new wax.leaf.connector(tilejson));
-  wax.leaf.interaction()
-    .map(map)
-    .tilejson(tilejson)
-    .on(wax.tooltip().animate(true).parent(map._container).events());
-  wax.tooltip()
-	.animate(true)
-	.parent()
     tilejson.minzoom = 2;
     tilejson.maxzoom = 7;
+    map.addLayer(new wax.leaf.connector(tilejson));
+    interaction.tilejson(tilejson);
   });
 }
 
