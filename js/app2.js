d931a640afa622ef9fb61ba4788122f46f66f88d
kleinjos <git@bf.33mail.com>
2012-03-25 21:51:13 -0400

    updated to v3

--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,6 @@
 var map;
 
-var url = 'http://api.tiles.mapbox.com/v2/';
+var url = 'http://api.tiles.mapbox.com/v3/';
 var baselayer = "occupy.wht-us-base"
 var activelayer = "occupy.4q11-delinq";
 
@@ -18,7 +18,7 @@ wax.tilejson(url + baselayer + ',' + activelayer + ',occupy.state-lines' + '.jso
 });
 
 function refreshMap(layers) {
-  wax.tilejson('http://api.tiles.mapbox.com/v2/' + layers + ',occupy.state-lines' + '.jsonp', function (tilejson) {
+  wax.tilejson('http://api.tiles.mapbox.com/v3/' + layers + ',occupy.state-lines' + '.jsonp', function (tilejson) {
     map.addLayer(new wax.leaf.connector(tilejson));
   wax.leaf.interaction()
     .map(map)
@@ -33,20 +33,6 @@ function refreshMap(layers) {
 }
 
 $(document).ready(function () {
-
-  // create a new UI Slider. Requires jQuery version 1.8
-  // http://docs.jquery.com/UI/API/1.8/Slider
-  $("#slider").slider({min:1999, max: 2011});
-
-  // refresh the map every time the slider changes
-  $("#slider").bind( "slidechange", function(event, ui) {
-  	// occupy.4q11-delinq
-  	var value = $("#slider").slider("value") + "";
-  	var year = value.substring(2,4);
-  	var activeLayer = "occupy.4q" + year + "-delinq";
-  	var layers = [baselayer, activeLayer];
-  	refreshMap(layers);
-  });
 
   $('ul.layers li a').click(function (e) {
     e.preventDefault();
